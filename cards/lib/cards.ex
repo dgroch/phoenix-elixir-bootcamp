19dbb81a8f38ca850ca5ea10411f89bb4828c5b1
Dan <dan@heroboyfriend.com>
2017-01-15 07:45:50 +0000

    Section 3 : Testing and Documentation

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,5 +1,12 @@
 defmodule Cards do
-  # create_deck() :: list
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+
+  @doc """
+    Creates a deck of cards
+  """
   def create_deck do
     values = ["Ace",    "Two",    "Three",  "Four", "Five", "Six",
               "Seven",  "Eight",  "Nine",   "Ten",  "Jack", "Queen",
@@ -10,19 +17,66 @@ defmodule Cards do
     for suit <- suits, value <- values, do: "#{value} of #{suit}"
   end
 
-  # shuffle(list) :: list
+  @doc """
+    Returns a shuffled deck of cards
+  """
   def shuffle(deck) do
     Enum.shuffle(deck) 
   end
 
-  # contains?(list, string) :: boolean
+  @doc """
+    Checks whether a deck of cards contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
-  # deal(list, integer) :: {list, list}
+  @doc """
+    Divides a deck into a hand and the remainder of the deck. The `hand_size` argument indicates how many cards should be in the hand
+    
+  ## Examples
+      
+      iex> deck = Cards.create_deck
+      iex> hand  = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
-    { hand, rest_of_deck} = Enum.split(deck, hand_size)
+    {hand, _rest} = Enum.split(deck, hand_size)
     hand
+  end
+
+  @doc """
+    Save a deck of cards to the filesystem
+  """
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  @doc """
+    Loads a deck of cards from the filesystem
+  """
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  # create_hand(hand_size) :: integer -> [ string, ...]
+  @doc """
+    Creates a hand of cards. The `hand_size` arguments specifies how many cards sohuld be in the hand.
+  """
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -5,15 +5,15 @@ defmodule CardsTest do
   @deck Cards.create_deck
 
   test "it creates a deck of cards" do
-    assert Enum.count(@deck) == 52
+    assert length(@deck) == 52
     assert Cards.contains?(@deck, "Ace of Spades")
     refute Cards.contains?(@deck, "Hello, World!")
   end
 
   test "it shuffles a deck of cards" do
     shuffled_deck = Cards.shuffle(@deck)
-    assert Enum.count(shuffled_deck) == 52
-    assert @deck !== shuffled_deck
+    assert length(shuffled_deck) == 52
+    refute shuffled_deck == @deck
   end
 
   test "it checks whether a list contains a card" do
@@ -21,10 +21,24 @@ defmodule CardsTest do
     refute Cards.contains?(@deck, "Hello, World!")
   end
 
-  test "deals a deck of cards" do
-    hand_five = Cards.deal(@deck, 5)
-    hand_ten = Cards.deal(@deck, 10)
-    assert Enum.count(hand_five) == 5
-    assert Enum.count(hand_ten) == 10
+  test "it deals a deck of cards" do
+    hand = Cards.deal(@deck, 5)
+    assert length(hand) == 5
+  end
+
+  test "it saves a deck to the filesystem" do
+    result = Cards.save(@deck, "test_file")
+    assert result == :ok
+  end
+
+
+  test "it loads a deck from the filesystem" do
+    deck = Cards.load("test_file")
+    assert length(deck) == 52
+  end
+
+  test "it creates a deck, shuffles it and deals a hand" do
+    hand = Cards.create_hand(10)
+    assert length(hand) == 10
   end
 end
