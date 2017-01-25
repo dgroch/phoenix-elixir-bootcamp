ea6d9c12f62faaefb174226bd5fbbce04de9514f
Dan <dan@heroboyfriend.com>
2017-01-25 20:39:59 +0000

    Section 9 : Models in Phoeniex

---
+++ discuss/web/models/topic.ex
@@ -0,0 +1,18 @@
+defmodule Discuss.Topic do
+  use Discuss.Web, :model
+
+  schema "topics" do
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
+end
